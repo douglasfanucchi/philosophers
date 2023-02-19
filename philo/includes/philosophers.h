a12622d937a0db9d55a46aab35fd9be58fe27f5d
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-19 10:41:24 -0300

    initial commit

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfanucch <dfanucch@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/19 10:40:19 by dfanucch          #+#    #+#             */
+/*   Updated: 2023/02/19 10:40:19 by dfanucch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+#endif
