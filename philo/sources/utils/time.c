f9ce6272f194d7219d0c4ebdb69224a39a554bec
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-20 14:22:23 -0300

    adjust timestamp to get more precision

--- philo/sources/utils/time.c
+++ philo/sources/utils/time.c
@@ -17,7 +17,7 @@ time_t	get_timestamp(void)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000);
+	return (tv.tv_sec * 1000 + (tv.tv_usec / 1000));
 }
 
 time_t	get_time(t_table table)
