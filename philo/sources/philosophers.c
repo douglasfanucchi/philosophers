133dc460b55c1810e915217bb672536c69a95c67
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-02-21 21:24:40 -0300

    validate program arguments

--- philo/sources/philosophers.c
+++ philo/sources/philosophers.c
@@ -12,5 +12,33 @@
 
 #include <philosophers.h>
 
-int	main()
-{}
+static char	has_valid_numbers(char **args)
+{
+	while (*args)
+	{
+		if (ft_atoi(*args) <= 0)
+			return (0);
+		args++;
+	}
+	return (1);
+}
+
+static char	is_valid(int argc, char **args)
+{
+	if (argc != 5 && argc != 6)
+		return (0);
+	if (!has_valid_numbers(args))
+		return (0);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_table	table;
+
+	if (!is_valid(argc, argv + 1))
+	{
+		write(2, "Invalid parameters\n", 19);
+		return (1);
+	}
+}
